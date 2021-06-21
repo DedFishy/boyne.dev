d080ee17109fe9f0be6a83289a51d1ee22f05ed9
PythonPower312 <54646090+SuperBoyne@users.noreply.github.com>
2021-06-21 22:14:44 +0000

    HUGE update!

--- js/home.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 600);
-  });
-
-['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){
-	document.body.style[prefix + 'transform'] = 'rotate(180deg)';
-});
