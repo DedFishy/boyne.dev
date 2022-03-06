9efac56a94fd089327112a304c126000c5c64077
Boyne G <54646090+SuperBoyne@users.noreply.github.com>
2022-03-06 04:13:12 +0000

    Improved the text, removed some stuff, added some stuff, and redid a bunch of the CSS

--- js/home.js
+++ js/home.js
@@ -1,8 +1,9 @@
 function quake() 
 { 
   ['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){
-	Array.prototype.slice.call(document.querySelectorAll('div,p,span,img,a,body')).map(function(el){
-		el.style[prefix + 'transform'] = 'rotate(' + (Math.floor(Math.random() * 100) - 1) + 'deg)';
+	Array.prototype.slice.call(document.querySelectorAll('div,p,span,img,a')).map(function(el){
+    el.style["transition"] = "1000s";
+		el.style[prefix + 'transform'] = 'rotate(' + (Math.floor(Math.random() * 1000) - 1) + 'deg)';
 	});
 });
 } 
