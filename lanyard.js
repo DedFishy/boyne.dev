60ab668f6beda2180dcaf676c4444bf42940e0cc
DedFishy <boynegregg312@gmail.com>
2024-10-28 17:54:56 -0400

    initial

--- lanyard.js
+++ lanyard.js
@@ -1,14 +1,10 @@
+const LANYARD_ENABLED = false;
+
 const lanyard_url = "https://api.lanyard.rest/v1/users/645264167623983124";
 
 const online_status_element = document.getElementById("online-status");
-const online_status_image = document.getElementById("online-status-image");
-const online_status_description = document.getElementById("online-status-description");
-const online_status_header = document.getElementById("online-status-header");
-const online_status_subheader = document.getElementById("online-status-subheader");
-const online_status_state = document.getElementById("online-status-state");
-const online_status_details = document.getElementById("online-status-details");
-
-const header_quote = document.getElementById("header-quote");
+const spotify_song_element = document.getElementById("lanyard-track-name");
+const activity_element = document.getElementById("lanyard-activity-name");
 
 async function get_lanyard_status() {
     let request_result = await fetch(lanyard_url);
@@ -19,51 +15,39 @@ async function get_lanyard_status() {
 function construct_online_status(json) {
     // Online/Offline indicator
     if (json["discord_status"] == "online") {
-        online_status_header.innerText = "online";
-        online_status_subheader.innerText = "just hanging out";
-        online_status_image.classList.add("online");
+        online_status_element.className = "online"
         
     } else if (json["discord_status"] == "idle") {
-        online_status_header.innerText = "idle";
-        online_status_image.classList.add("idle");
+        online_status_element.className = "idle"
     } else if (json["discord_status"] == "dnd") {
-        online_status_header.innerText = "do not disturb";
-        online_status_image.classList.add("dnd");
+        online_status_element.className = "dnd"
     }
     else {
-        online_status_header.innerText = "offline"
-        online_status_subheader.innerText = "touching grass"
-        online_status_image.classList.add("offline");
+        online_status_element.className = "offline"
     }
 
     if (json["activities"].length != 0) {
 
+        online_status_element.className = "activity"
+
         for (let activity of json["activities"]) {
 
             if (activity["type"] == 4) {
-                header_quote.innerText = activity["state"];
+                //activity_element.innerText = activity["state"];
             } else {
-                online_status_subheader.innerText = activity["name"].toLowerCase()
-                if (activity["state"]) { online_status_state.innerText = activity["state"].toLowerCase() };
-                online_status_details.innerText = activity["details"].toLowerCase()
+                //online_status_element.innerText = activity["name"].toLowerCase()
+                //if (activity["state"]) { online_status_element.innerText = activity["state"].toLowerCase() };
+                //online_status_element.innerText = activity["details"].toLowerCase()
+                activity_element.innerText = activity["name"];
             }
 
         }
-
-    
-    } else if (json["active_on_discord_desktop"] | json["active_on_discord_web"]) {
-
-        online_status_subheader.innerText = "on the computer"
-
-    } else if (json["active_on_discord_mobile"]) {
-        online_status_subheader.innerText = "on mobile"
     }
 
     if (json["spotify"] != null) {
+        online_status_element.className = "spotify"
         const spotify = json["spotify"];
-        online_status_image.src = spotify["album_art_url"];
-        online_status_header.innerText = "spotify"
-        online_status_subheader.innerText = (spotify["song"] + "\nby " + spotify["artist"]).toLowerCase()
+        spotify_song_element.innerHTML = "<strong>" + spotify["song"] + "</strong> by <strong>" + spotify["artist"] + "</strong>"
     }
 
     console.log(json);
@@ -71,13 +55,9 @@ function construct_online_status(json) {
 
 function update_lanyard_loop() {
     get_lanyard_status().then(json => {
-        online_status_header.innerText = "";
-        online_status_subheader.innerText = "";
-        online_status_state.innerText = "";
-        online_status_details.innerText = "";
         construct_online_status(json);
         setTimeout(update_lanyard_loop, 10000);
     });
 }
 
-update_lanyard_loop();
+if (LANYARD_ENABLED) update_lanyard_loop();
